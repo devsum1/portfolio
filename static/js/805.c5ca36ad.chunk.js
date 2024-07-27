59315d3daf6c2a7ea04c847f6acedc24bbc8c7ba
sumit-cashify <sumit.singh1@cashify.in>
2024-07-27 11:28:25 +0530

    Updates

---
+++ static/js/805.c5ca36ad.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[805],{2805:(i,t,a)=>{a.r(t),a.d(t,{default:()=>n});a(2483);var e=a(806),o=a(6723);const n=i=>{let{children:t}=i;return(0,o.jsx)(e.P.div,{initial:{opacity:0},animate:{opacity:1},exit:{opacity:0},transition:{duration:.5},children:t})}}}]);
+//# sourceMappingURL=805.c5ca36ad.chunk.js.map
